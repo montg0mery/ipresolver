9d9103978405785e37a7a691f1840e667f300c7d
montg0mery <66806587+montg0mery@users.noreply.github.com>
2020-08-11 18:27:30 -0300

    Update ipresolver.sh

--- ipresolver.sh
+++ ipresolver.sh
@@ -6,6 +6,6 @@ for host in $list
 do
         ips=$(dig "$host" +short 2>/dev/null | grep -oP "([0-9]{1,3}\.){3}[0-9]{1,3}")
         line=$(echo $ips | tr -d '\n' | tr ' ' ', ' )
-        echo "$host : $ips"
-        echo "$host : $ips" >> ip.txt
+        echo "$host : $line"
+        echo "$host : $line" >> ip.txt
 done
