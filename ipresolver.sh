ef9b88cf50a32d0d7ba9fe4187ae7e3c69525242
montg0mery <66806587+montg0mery@users.noreply.github.com>
2020-08-16 13:08:44 -0300

    Update ipresolver.sh

--- ipresolver.sh
+++ ipresolver.sh
@@ -6,6 +6,20 @@ for host in $list
 do
         ips=$(dig "$host" +short 2>/dev/null | grep -oP "([0-9]{1,3}\.){3}[0-9]{1,3}")
         line=$(echo $ips | tr -d '\n' | tr ' ' ', ' )
-        echo "$host : $line"
-        echo "$host : $line" >> ip.txt
+
+        first_ip=$(echo $line | awk -F',' '{print $1}')
+        if [ -z "$first_ip" ]
+        then
+                :
+        else
+                echo ""
+                echo "scanning $host -> $first_ip"
+                echo ""
+                masscan $first_ip -p 1-65535 -oG tmp.grep
+                ports=$(cat tmp.grep | grep tcp | awk -F'Ports: ' '{print $2}' | cut -d/ -f1 | tr '\n' ',')
+                rm tmp.grep
+        fi
+
+        echo "$host : $line : $ports"
+        echo "$host : $line : $ports" >> ip.txt
 done
