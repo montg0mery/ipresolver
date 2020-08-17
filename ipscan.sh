7380f65c6e6e394580a9124af1623510ce6ceaed
montg0mery <66806587+montg0mery@users.noreply.github.com>
2020-08-17 14:10:29 -0300

    Update ipscan.sh
    made it faster

--- ipscan.sh
+++ ipscan.sh
@@ -5,21 +5,22 @@ list=$(cat $1)
 for host in $list
 do
         ips=$(dig "$host" +short 2>/dev/null | grep -oP "([0-9]{1,3}\.){3}[0-9]{1,3}")
-        line=$(echo $ips | tr -d '\n' | tr ' ' ', ' )
-
-        first_ip=$(echo $line | awk -F',' '{print $1}')
-        if [ -z "$first_ip" ]
+        if [ -z "$ips" ]
         then
-                :
+                echo "$host : :" >> ip.txt
         else
+                line=$(echo $ips | tr -d '\n' | tr ' ' ', ' )
+       
+                first_ip=$(echo $line | awk -F',' '{print $1}')
                 echo ""
                 echo "scanning $host -> $first_ip"
                 echo ""
-                masscan $first_ip -p 1-65535 -oG tmp.grep
+                masscan $first_ip -p 1-65535 -oG tmp.grep --max-rate 1000
                 ports=$(cat tmp.grep | grep tcp | awk -F'Ports: ' '{print $2}' | cut -d/ -f1 | tr '\n' ',')
                 rm tmp.grep
+        
+                echo "$host : $line : $ports"
+                echo "$host : $line : $ports" >> ip.txt
         fi
 
-        echo "$host : $line : $ports"
-        echo "$host : $line : $ports" >> ip.txt
 done
